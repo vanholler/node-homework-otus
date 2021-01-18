ed96b47b9fa51e3b4f67882c493c5781813b9a8c
Mikhnevich <artmartinov@mail.ru>
2021-01-18 07:49:05 +0300

    add create big file and sorting

---
+++ streams/bin/join.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const fs = require('fs')
+
+function joinFiles(srcDir, files) {
+  console.log('--------------------------->', srcDir, files)
+  return new Promise((res, rej) => {
+    const resultFilePath = path.join(srcDir, 'result.txt')
+    
+    const writable = fs.createWriteStream(resultFilePath)
+
+    let currentStream = 0
+
+    const streams = files.map((file, index) => {
+      const readable = fs.createReadStream(file)
+
+      readable.on('data', (chunk) => {
+        writable.write(chunk)
+      })
+
+      readable.on('end', () => {
+        if (streams[index + 1]) {
+          streams[index + 1].resume()
+        }   
+      })
+
+      return readable
+    })
+
+    res(resultFilePath)
+  })
+}
+
+module.exports = joinFiles
